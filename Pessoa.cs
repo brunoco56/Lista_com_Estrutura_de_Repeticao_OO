3df5b90ac04c92abd9ee94b7265745384c755e95
Bruno <brunoco56@hotmail.com>
2021-02-26 16:42:59 -0300

    Adicionar arquivos de projeto.

---
+++ Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arduino
+{
+    public class Pessoa
+    {
+        public Pessoa()
+        {
+
+        }
+        public int idade { get; set; }
+        public string nome { get; set; }
+
+        public string andar { get; set; }
+
+        public string andar1(int idade)
+        {
+            if (idade > 5)
+            {
+                this.andar = "Anda demais da conta ";
+            }
+            else
+            {
+                this.andar = "Anda não é muito pequena ";
+            }
+            return andar;
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arduino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char resposta ='N';            
+           DateTime data = DateTime.Now;            
+            
+            List<Pessoa> usuarios = new List<Pessoa>();
+
+            do
+            {
+                Pessoa pessoa = new Pessoa();               
+
+                Console.WriteLine("*************");
+                Console.WriteLine("Digite seu nome:");
+                pessoa.nome = Console.ReadLine();
+                Console.WriteLine("Digite o ano do seu Nascimento:");
+                pessoa.idade = data.Year - int.Parse(Console.ReadLine());
+                pessoa.andar1(pessoa.idade);
+                Console.WriteLine("***************");
+                usuarios.Add(pessoa);
+                Console.WriteLine("Deseja finalizar? S/N ");
+                resposta = char.Parse(Console.ReadLine());
+                Console.WriteLine("***********************");                
+
+            } while (resposta == 'N');
+
+            foreach (var item in usuarios)
+            {
+                Console.WriteLine(item.nome);
+                Console.WriteLine(item.idade);
+                Console.WriteLine(item.andar);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
